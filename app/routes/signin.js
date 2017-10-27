927345f4a3b7a6c11249aa4238f34b6abb64d24f
chiamingpan <jessicapan1224@gmail.com>
2017-10-27 00:59:03 -0700

    Update signin page

--- app/routes/signin.js
+++ app/routes/signin.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend({});
+
+$(window, document, undefined).ready(function () {
+
+    $('input').blur(function () {
+        var $this = $(this);
+        if ($this.val())
+            $this.addClass('typed');
+        else
+            $this.removeClass('typed');
+    });
 });
