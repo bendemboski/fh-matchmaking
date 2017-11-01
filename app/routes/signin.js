9819809cede205b2bbb31f0d03e0fa231508925e
Ben Demboski <ben@turbopatent.com>
2017-10-31 21:42:44 -0700

    Implement component for running signing page Javascript

--- app/routes/signin.js
+++ app/routes/signin.js
@@ -1,15 +1,3 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({});
-
-
-Ember.$(window, document, undefined).ready(function () {
-
-    Ember.$('input').blur(function () {
-        var $this = Ember.$(this);
-        if ($this.val())
-            $this.addClass('typed');
-        else
-            $this.removeClass('typed');
-    });
-});
