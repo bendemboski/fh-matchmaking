1330ec807f184c4a598d171b156428b04ea7178b
chiamingpan <jessicapan1224@gmail.com>
2017-11-02 04:08:28 -0700

    Add new file of homepage_host

--- app/router.js
+++ app/router.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+    location: config.locationType,
+    rootURL: config.rootURL
 });
 
-Router.map(function() {
+Router.map(function () {
   this.route('signin');
-  this.route('about');
+  this.route('homepage_host');
 });
 
 export default Router;
