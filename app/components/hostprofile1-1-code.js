3a97629a7ebd059cb76c64b4166c65641f4c6765
Ben Demboski <ben@turbopatent.com>
2017-11-14 12:47:53 -0800

    Move profile pic code into code component

--- app/components/hostprofile1-1-code.js
+++ app/components/hostprofile1-1-code.js
@@ -3,9 +3,26 @@ import Ember from 'ember';
 const { Component } = Ember;
 
 export default Component.extend({
+  profileSrc: '/assets/images/icon_user.png',
+
+
   didInsertElement() {
     this._super(...arguments);
 
     // let $ = this.$.bind(this);
+  },
+
+  actions: {
+    chooseProfilePic() {
+      this.$('#imageUpload').click();
+    },
+
+    onProfilePic(e) {
+      let files = e.target.files || [];
+      let file = files[0];
+      if (file) {
+        this.set('profileSrc', window.URL.createObjectURL(file));
+      }
+    }
   }
 });
