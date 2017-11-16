ead7ce7c45e4f161d947b212eb8e7f1bc0bdab62
chiamingpan <jessicapan1224@gmail.com>
2017-11-16 11:11:26 -0800

    Adds hostprofile1-4

---
+++ app/components/hostprofile1-4-code.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+const { Component } = Ember;
+
+export default Component.extend({
+  didInsertElement() {
+    this._super(...arguments);
+
+    // let $ = this.$.bind(this);
+  }
+});
