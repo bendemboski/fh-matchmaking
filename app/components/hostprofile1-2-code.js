068205e17f0689ea68c999733c00834490c95e5f
Ben Demboski <ben@turbopatent.com>
2017-11-16 21:00:45 -0800

    Move the add/remove logic into Ember code

--- app/components/hostprofile1-2-code.js
+++ app/components/hostprofile1-2-code.js
@@ -3,24 +3,23 @@ import Ember from 'ember';
 const { Component } = Ember;
 
 export default Component.extend({
+  adultCount: 1,
+  kidCount: 0,
+  petCount: 0,
+
   didInsertElement() {
     this._super(...arguments);
 
     // let $ = this.$.bind(this);
+  },
+
+  actions: {
+    increment(propName) {
+      this.incrementProperty(propName);
+    },
+
+    decrement(propName) {
+      this.decrementProperty(propName);
+    }
   }
 });
-
-// NumberIncrementer
-
-$(document).on('click','.value-control',function(){
-    var action = $(this).attr('data-action')
-    var target = $(this).attr('data-target')
-    var value  = parseFloat($('[id="'+target+'"]').val());
-    if ( action == "plus" ) {
-      value++;
-    }
-    if ( action == "minus" && value > 0 ) {
-      value--;
-    }
-    $('[id="'+target+'"]').val(value)
-})
