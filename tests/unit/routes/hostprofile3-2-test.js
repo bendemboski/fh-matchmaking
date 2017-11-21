c376c8f8ee4df3e1107444d5d3b4dcce7491903b
chiamingpan <jessicapan1224@gmail.com>
2017-11-21 00:28:04 -0800

    Adds host profile3-2

---
+++ tests/unit/routes/hostprofile3-2-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:hostprofile3-2', 'Unit | Route | hostprofile3 2', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
