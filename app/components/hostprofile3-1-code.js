e2155a060a81032ea6b8edc87fbc22f42d162499
chiamingpan <jessicapan1224@gmail.com>
2017-12-07 17:50:22 -0800

    Modifies hostprofile3-1 js file

--- app/components/hostprofile3-1-code.js
+++ app/components/hostprofile3-1-code.js
@@ -9,11 +9,3 @@ export default Component.extend({
     // let $ = this.$.bind(this);
   }
 });
-
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
