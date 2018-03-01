5fd35a3d400d6870ceb356c2a531257f27f14930
chiamingpan <jessicapan1224@gmail.com>
2018-02-28 22:46:39 -0800

    Sync all pages names-part2

--- tests/unit/routes/previewpage-resident-test.js
+++ tests/unit/routes/previewpage-resident-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:previewpage_resident', 'Unit | Route | previewpage resident', {
+moduleFor('route:previewpage-resident', 'Unit | Route | previewpage resident', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
