ba4f4ff8183dfd79d700b2a932512eae0982311b
chiamingpan <jessicapan1224@gmail.com>
2018-05-06 17:57:18 -0700

    Adds show/hide functions for the filters in the matchingpage

--- app/components/matching-page.js
+++ app/components/matching-page.js
@@ -1,12 +1,13 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
-
-export default Component.extend({
+export default Ember.Component.extend({
+/* Number Incrementation */
   formName: "hostform",
   adultCount: 1,
   kidCount: 0,
   petCount: 0,
+  familyPanelShow: false,
+  morePanelShow: false,
 
   didInsertElement() {
     this._super(...arguments);
@@ -21,6 +22,32 @@ export default Component.extend({
 
     decrement(propName) {
       this.decrementProperty(propName);
+    },
+
+    familyFilterShow() {
+      if (this.familyPanelShow) {
+        this.set('familyPanelShow', false);
+      } else {
+          this.set('familyPanelShow', true);
+      }
+      this.set('morePanelShow', false);
+    },
+
+    familyFilterHide() {
+      this.set('familyPanelShow', false);
+    },
+
+    moreFilterShow() {
+      if(this.morePanelShow){
+      this.set('morePanelShow', false);
+    } else{
+      this.set('morePanelShow', true);
     }
+    this.set('familyPanelShow', false)
+    },
+
+    moreFilterHide() {
+      this.set('morePanelShow', false);
+    },
   }
 });
