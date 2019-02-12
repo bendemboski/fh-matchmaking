5075e047b46f15cd0aa0303b142e8df342ec0eee
Ben Demboski <ben@turbopatent.com>
2019-02-11 23:31:32 -0800

    Deploy to production on successful master build

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,6 +3,8 @@
 echo "decrypting credentials..."
 mkdir -p ~/.aws
 openssl aes-256-cbc -K $encrypted_388f6c49a1a2_key -iv $encrypted_388f6c49a1a2_iv -in ci/aws-credentials.enc -out ~/.aws/credentials -d
+echo "deploying to development..."
+yarn deploy --activate development
 echo "deploying to staging..."
 yarn deploy --activate staging
 echo "deploying to production..."
