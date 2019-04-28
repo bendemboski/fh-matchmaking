fa43d0148ebd87a0c78158b9ff4e806edbcb8962
chiamingpan <jessicapan1224@gmail.com>
2019-04-28 13:36:52 -0700

    Updated testimonial pics and host profile

--- app/components/profile-page/testimonial-pic.js
+++ app/components/profile-page/testimonial-pic.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: [ 'testimonial-pic' ]
+  classNames: [ 'testimonial-pic-kim-and-dan' , 'testimonial-pic-lex-and-marjon' , 'testimonial-pic-najja' ]
 });
