5acb55685fb136badeebcc5e9cf7de80bf81bda1
Ben Demboski <ben@turbopatent.com>
2019-05-21 21:24:12 -0700

    Update testimonial pic styling
    Switch back to using components, and in addition to the generic `testimonial-pic` class that does all the styling, use a second picture-specific class name to set the background to the correct image.

--- app/components/profile-page/testimonial-pic.js
+++ app/components/profile-page/testimonial-pic.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: [ 'testimonial-pic-kim-and-dan' , 'testimonial-pic-lex-and-marjon' , 'testimonial-pic-najja' ]
+  classNames: [ 'testimonial-pic' ]
 });
